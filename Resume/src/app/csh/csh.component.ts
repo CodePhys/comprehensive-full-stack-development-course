a4a4054fefd9378115256243f73ebee3dcb4b2d4
Marco Lissandrello <m.lissandrello@sister.it>
2023-11-29 17:39:12 +0100

    marco-marco22 merge

--- Resume/src/app/csh/csh.component.ts
+++ Resume/src/app/csh/csh.component.ts
@@ -6,10 +6,6 @@ import {Component} from '@angular/core';
   templateUrl: './csh.component.html',
   styleUrls: ['./csh.component.css'],
 })
-<<<<<<< HEAD
-export class CShComponent {
-=======
-export class CshComponent {
->>>>>>> dcae8c16634f245c114b609f5d39b374c7ddc5d4
+
 
 export class CshComponent {}
