b9b4ea6bb191b1424efd89104c14ae8ded2f0031
Simona <prova@miao.it>
2023-11-29 12:56:24 +0100

    aggiornamenti homepage-navbar

--- Resume/src/app/app.component.ts
+++ Resume/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Resume';
+  title = 'Corso Randstad';
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
--- Resume/src/app/app.module.ts
+++ Resume/src/app/app.module.ts
@@ -13,6 +13,14 @@ import { RctComponent } from './rct/rct.component';
 import { SqlComponent } from './sql/sql.component';
 import { GitComponent } from './git/git.component';
 
+//Angular Materials
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+//Altri styles
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 @NgModule({
   declarations: [
@@ -81,7 +89,12 @@ import { GitComponent } from './git/git.component';
 
       ]
     ),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    FlexLayoutModule
     
   ],
   providers: [],
