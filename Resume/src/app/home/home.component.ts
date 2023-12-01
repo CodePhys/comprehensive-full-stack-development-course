ded97bfe701cf624f71583c64eaaf267a20774d8
Simona <prova@miao.it>
2023-12-01 12:53:02 +0100

    fatta home

--- Resume/src/app/home/home.component.ts
+++ Resume/src/app/home/home.component.ts
@@ -1,39 +1,30 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent {
   cards = [
     
     {
-      title: 'Card 2',
-      image: 'https://via.placeholder.com/300',
-      content: 'Content for Card 2'
+      title: 'Benvenuti al nostro riassunto completo del corso di sviluppo software',
+      image: '/assets/images/home.png',
+      content: 'Una panoramica che offre un\'occhiata ravvicinata agli argomenti chiave che abbiamo esplorato e padroneggiato insieme. Nel corso di questa avventura formativa, abbiamo navigato attraverso una serie di tecnologie e linguaggi di programmazione fondamentali nel panorama attuale dello sviluppo web e software.Da C#, noto per la sua potenza e versatilità nell\'ambito dello sviluppo .NET, a SQL Server, un sistema di gestione di database robusto e affidabile, abbiamo gettato le basi per una comprensione solida delle tecnologie back-end.'
     },
     {
-      title: 'Card 3',
-      image: 'https://via.placeholder.com/300',
-      content: 'Content for Card 3'
+      title: 'Benvenuti al nostro riassunto completo del corso di sviluppo software',
+      image: '/assets/images/home.png',
+      content: 'Non ci siamo fermati qui: abbiamo esplorato ASP.NET MVC, un framework potente per costruire applicazioni web dinamiche e ricche di funzionalità, garantendo un approfondimento sulle tecniche di sviluppo lato server. Abbiamo poi virato verso il front-end, dove JavaScript e TypeScript ci hanno aperto le porte alla programmazione di interfacce utente interattive e moderne. Infine, ci siamo immersi nel mondo dei framework front-end Angular e React, due giganti nel campo dello sviluppo di single-page applications e interfacce utente reattive.'
     },
     {
-      title: 'Card 4',
-      image: 'https://via.placeholder.com/300',
-      content: 'Content for Card 4'
+      title: 'Benvenuti al nostro riassunto completo del corso di sviluppo software',
+      image: '/assets/images/home.png',
+      content: "Questo riassunto serve non solo come un promemoria delle competenze acquisite, ma anche come una guida per ulteriori esplorazioni e apprendimenti. Che siate sviluppatori in erba o esperti che cercano di rinfrescare le loro conoscenze, queste pagine rappresentano un punto di riferimento essenziale per il vostro viaggio nel mondo dello sviluppo software."
     },
-    {
-      title: 'Card 5',
-      image: 'https://via.placeholder.com/300',
-      content: 'Content for Card 5'
-    },
-    {
-      title: 'Card 6',
-      image: 'https://via.placeholder.com/300',
-      content: 'Content for Card 6'
-    },
+  
     // Add more cards as needed
   ];
 
